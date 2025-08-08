4c6254ee78c433a8f353fc832bbc2750ec38bf3c
Balaji S <balajiselvaraj12@gmail.com>
2025-08-08 14:07:34 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-=======
-  base: '/',
->>>>>>> 69880766a2b89f3e19be86ca5ceaee6dd5c98692
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-  },
   optimizeDeps: {
-    include: ['react', 'react-dom'],
+    exclude: ['lucide-react'],
   },
 });
